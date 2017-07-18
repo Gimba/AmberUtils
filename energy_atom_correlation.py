c1c2d04f3a626c5867e74fc57d96c976259dcc18
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-07-18 13:06:39 +0100

    read in data

--- energy_atom_correlation.py
+++ energy_atom_correlation.py
@@ -10,5 +10,17 @@ def main(argv):
     parser.add_argument('outfile', help='outfile')
     args = parser.parse_args()
 
+    energies = []
+    with open(args.energy, 'r') as f:
+        for line in f:
+            if(line.isdigit()):
+                energies.append(float(line))
+
+    table = []
+    with open(args.infile, 'r') as f:
+        f.readline()
+        for line in f:
+            table.append(line)
+
 if __name__ == "__main__":
     main(sys.argv)
