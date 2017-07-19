4daf655953330193c3ff8471f99bcd90718cbda4
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-07-19 17:50:21 +0100

    altered energy_atom_plot.py a little to fit the task here

--- plot_distance_corrs.py
+++ plot_distance_corrs.py
@@ -8,6 +8,31 @@ def main(argv):
     parser = argparse.ArgumentParser(description='Plot energy atom correlation interactions.')
     parser.add_argument('infile', help='csv file created by energy_atom_correlation.py')
     args = parser.parse_args()
+    values = []
+
+    with open(args.infile, 'r') as f:
+        for line in f:
+            temp = line.split(',')
+            temp[1] = float(temp[1])
+            temp[2] = float(temp[2][:-1])
+            values.append(temp)
+
+    values = sorted(values, key=lambda x: (x[1]))
+
+    outfile = str(args.infile).split('.')[0]
+
+    neg = []
+    xticks = []
+    for i in range(0, len(values)):
+        item = values[i]
+        neg.append(item[1])
+        xticks.append(item[0])
+
+    plt.figure(figsize=(25, 15))
+    plt.xticks(range(len(xticks)), xticks, size='small', rotation='45', ha='right')
+    plt.plot(neg)
+    plt.ylabel("correlation coefficient")
+    plt.savefig(outfile + ".png")
 
 if __name__ == "__main__":
     main(sys.argv)
