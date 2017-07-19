75b62b7a759d64f9b8bc3119ef6d0e19fcbae698
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-07-19 10:13:19 +0100

    read in csv file

--- energy_atom_plot.py
+++ energy_atom_plot.py
@@ -6,5 +6,16 @@ def main(argv):
     parser.add_argument('infile', help='csv file created by energy_atom_correlation.py')
     args = parser.parse_args()
 
+    values = []
+
+    with open(args.infile, 'r') as f:
+        for line in f:
+            temp = line.split(',')
+            temp[1] = float(temp[1])
+            temp[2] = float(temp[2][:-1])
+            values.append(temp)
+
+    
+
 if __name__ == "__main__":
     main(sys.argv)
