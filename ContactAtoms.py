8d0245e0afe8973696f0263b44ad60d01d91d63b
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-07-21 17:41:45 +0100

    count atomic contacts

--- ContactAtoms.py
+++ ContactAtoms.py
@@ -30,6 +30,8 @@ def main(argv):
     parser.add_argument('mutated', help='mutated contacts')
     args = parser.parse_args()
 
+    # get contact count for unmutated structure
+
     contacts = []
 
     with open(args.contacts, 'r') as f:
@@ -50,6 +52,33 @@ def main(argv):
     for item in residues:
         contact_atoms.append([item, contacts.count(item)])
 
+
+    # get contact count for mutated structure
+
+    mutated = []
+
+    with open(args.mutated, 'r') as f:
+        for line in f:
+            if line[0] is not '#':
+                # get extra residue mutated
+                if "_:23@" not in line:
+                    line = line.split(' ')
+                    line = filter(None, line)
+                    line = line[1].split("_")[1]
+                    line = line.split('@')[0]
+                    line = line.replace(':', '')
+                    mutated.append(line)
+
+    residues = list(set(mutated))
+
+    mutated_atoms = []
+    for item in residues:
+        mutated_atoms.append([item, mutated.count(item)])
+
+    print contact_atoms
+    print mutated_atoms
+
+    
     # model 1:
     # remove contacts with itself select all lines with ':23@\l*\d*_:23@'
     # count atom contacts for each residue
