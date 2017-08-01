5f7871beafb4dda1d2c439c2ab0f451f112795bf
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-08-01 10:40:35 +0100

    add method to read pdb to list

---
+++ PdbHelper.py
@@ -0,0 +1,20 @@
+
+
+# method reading the 'ATOM' records of a pdb file and returns atom number, atom type, residue type, residue number and
+# coordinates in a list (e.g. [[1, N, CYX, 1, 43.390, 49.887, -62.005],[2, CA, ...], ...])
+def read_pdb_atoms(pdb_file):
+    out = []
+
+    with open(pdb_file, 'r') as f:
+        for line in f:
+            line = line.split()
+            if 'ATOM' in line[0]:
+                a_number = line[1]
+                a_type = line[2]
+                res_type = line[3]
+                res_number = line[4]
+                x = line[5]
+                y = line[6]
+                z = line[7]
+                out.append(a_number, a_type, res_type, res_number, x, y, z)
+    return out
