aedefc37b3019ab740bfb8cc26932e4f599acbae
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-08-01 10:08:33 +0100

    put create_contact_cpptraj in helper

--- CpptrajHelper.py
+++ CpptrajHelper.py
@@ -16,3 +16,19 @@ def read_cpptraj_contacts_data(file_name):
 
 def run_cpptraj(pdb, trajin, cpptraj_file):
     os.system('cpptraj -p ' + pdb + ' -y ' + trajin + ' -i ' + cpptraj_file)
+
+
+def create_contact_cpptraj(trajin, res1, res2):
+
+    cpptraj_file = trajin.split('.')[0] + "_contacts.cpptraj"
+    out_file = cpptraj_file.replace('cpptraj', 'dat')
+
+    with open(cpptraj_file, 'w') as f:
+        f.write('strip :WAT\nstrip @H*\nstrip @?H*\n')
+        for item1 in res1:
+            for item2 in res2:
+                f.write('nativecontacts :' + item1 + ' :' + item2 + ' writecontacts ' +
+                    out_file + ' distance 3.9\n')
+        f.write('go')
+
+    return [cpptraj_file, out_file]
