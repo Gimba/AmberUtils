078b4a8c6185fb231f9359942431efc94f9144f3
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2017-09-08 11:15:16 +0100

    introduced masks to contact calculation of types

--- CpptrajHelper.py
+++ CpptrajHelper.py
@@ -56,7 +56,7 @@ def create_contact_cpptraj(trajin, res1, res2, wat, hydro):
 
 
 # get contacts for atom types
-def create_contact_cpptraj_types(trajin, types, wat, hydro):
+def create_contact_cpptraj_types(trajin, types, mask1, mask2, wat, hydro):
     cpptraj_file = trajin.split('.')[0].strip("\"") + "_" + trajin.split('.')[1].split()[0] + "_contacts_types.cpptraj"
     out_file = cpptraj_file.replace('cpptraj', 'dat')
 
@@ -67,12 +67,8 @@ def create_contact_cpptraj_types(trajin, types, wat, hydro):
             f.write('strip @H*\nstrip @?H*\nstrip @Cl-\n')
 
         for item in types:
-            if item == 'O':
-                f.write('nativecontacts (:1-582)&(@O) :1-500000 writecontacts ' +
-                        out_file + ' distance 3.9\n')
-            else:
-                f.write('nativecontacts :*' + item + ' writecontacts ' +
-                    out_file + ' distance 3.9\n')
+            f.write('nativecontacts (' + mask1 + ')&(@' + item + ') :' + mask2 + ' writecontacts ' + out_file +
+                    ' distance 3.9\n')
         f.write('go')
 
     return [cpptraj_file, out_file]
